4fb8ce69d6d1aa93736df9bcca8cee0d0d5971fb
ngwesse elvis <elvisdemoprojects@outlook.com>
2020-11-18 17:41:00 +0000

    changes

--- src/profilesApi/profile_api/serializers.py
+++ src/profilesApi/profile_api/serializers.py
@@ -1,6 +1,29 @@
 from rest_framework import serializers
+from . import models
 
 class HelloSerializer(serializers.Serializer):
     """Serialises a name field for testing our view"""
 
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for our profile objects"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id','email','firstName','lastName','password')
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+
+        user = models.UserProfile(
+            email = validated_data['email'],
+            firstName = validated_data['firstName'],
+            lastName = validated_data['lastName'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- src/profilesApi/profile_api/views.py
+++ src/profilesApi/profile_api/views.py
@@ -3,6 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import  serializers 
 from rest_framework import status
+from rest_framework import viewsets
+from . import models
+from . import permissions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 # Create your views here.
 class HelloAPIView(APIView):
@@ -32,3 +38,78 @@ class HelloAPIView(APIView):
         else:
 
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Handels updating an object with pk=primary key"""
+
+        return Response({'method':'put'})
+
+
+    def patch(self, request, pk=None):
+        """Handels patching an object with pk=primary key"""
+
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        """Handels deleting an object with pk=primary key"""
+
+        return Response({'method':'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test api viewset"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, reque):
+        """This is going to return a hello message"""
+        a_viewse = [
+            'Uses acions (lis, cae, retrieve, updte, parial_upde)',
+            'Automaially mapsthurlssinroutrs',
+            'Proves morecnaliwithlssoe'
+        ]
+
+        return Response({'message':'Hello','a_viewset':a_viewse})
+    
+    def create(self, request):
+        """Create anew hello message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handels getting an object by its ID"""
+
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """Handels updating an object by its ID"""
+
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handels partial updating an object by its ID"""
+
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handels removing an object by its ID"""
+
+        return Response({'http_method':'Delete'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permissions_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('firstName','email',)
